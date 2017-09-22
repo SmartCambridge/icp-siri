6caafabd35f5d2efbcb3b7eb97938c90b415ce09
Jon Warbrick <jw35@cam.ac.uk>
2017-09-22 15:06:55 +0100

    Add support for start/end stop location and names
    This required a 'naptan' table in the database created
    by separate code.

--- extract_journeys.py
+++ extract_journeys.py
@@ -9,29 +9,39 @@ conn = sqlite3.connect("activity.sqlite")
 cur = conn.cursor()
 
 query = '''
-select Latitude,                    --  0
-       Longitude,                   --  1
-       OriginAimedDepartureTime,    --  2
-       OriginRef,                   --  3
-       DestinationRef,              --  4
-       DirectionRef,                --  5
-       OperatorRef,                 --  6
-       LineRef,                     --  7
-       VehicleRef,                  --  8
-       SerialNo,                    --  9
-       RecordedAtTime               -- 10
-  from activity
-  where date(RecordedAtTime) == "2017-04-26" and
-        latitude < 52.6 and
-        longitude > -0.2
-  order by OriginAimedDepartureTime,
-           OriginRef,
-           DestinationRef,
-           DirectionRef,
-           OperatorRef,
-           LineRef,
-           VehicleRef,
-           SerialNo
+select at.Latitude,                    --  0
+       at.Longitude,                   --  1
+       at.OriginAimedDepartureTime,    --  2
+       at.OriginRef,                   --  3
+       at.DestinationRef,              --  4
+       at.DirectionRef,                --  5
+       at.OperatorRef,                 --  6
+       at.LineRef,                     --  7
+       at.VehicleRef,                  --  8
+       at.SerialNo,                    --  9
+       at.RecordedAtTime,              -- 10
+       start.CommonName,               -- 11
+       start.LocalityName,             -- 12
+       start.Latitude,                 -- 13
+       start.Longitude,                -- 14
+       end.CommonName,                 -- 15
+       end.LocalityName,               -- 16
+       end.Latitude,                   -- 17
+       end.Longitude                   -- 18
+  from activity as at
+  left join naptan as start on at.OriginRef = start.ATCOCode
+  left join naptan as end on at.DestinationRef = end.ATCOCode
+  where date(at.RecordedAtTime) == "2017-08-30" and
+        at.latitude < 52.4 and
+        at.longitude > -0.2
+  order by at.OriginAimedDepartureTime,
+           at.OriginRef,
+           at.DestinationRef,
+           at.DirectionRef,
+           at.OperatorRef,
+           at.LineRef,
+           at.VehicleRef,
+           at.SerialNo
 '''
 
 cur.execute(query)
@@ -47,9 +57,13 @@ for row in rows:
         if journey:
             result.append(journey)
         journey = {}
-        journey['name'] = this
+        journey['name'] = ' '.join((row[2], row[3], row[12], row[11], row[4], row[16], row[15]) + (row[5:9]))
         journey['points'] = []
         journey['points_desc'] = []
+        journey['start'] = [row[13], row[14]]
+        journey['start_label'] = ', '.join((row[12], row[11]))
+        journey['end'] = [row[17], row[18]]
+        journey['end_label'] = ', '.join((row[16], row[15]))
     current = this
     journey['points'].append(row[0:2])
     journey['points_desc'].append(str(row[9]) + ' ' + row[10])
