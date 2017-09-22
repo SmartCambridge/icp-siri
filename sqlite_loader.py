ea60624da901656fa2f74edf7a8001db0c61559f
Jon Warbrick <jw35@cam.ac.uk>
2017-09-22 15:06:13 +0100

    Correct serial number handling; add checks for unexpected tags

--- sqlite_loader.py
+++ sqlite_loader.py
@@ -7,30 +7,38 @@ import sys
 import re
 
 ns = { 'siri': 'http://www.siri.org.uk/siri' }
+wanted = ['RecordedAtTime',
+           'ValidUntilTime',
+           'VehicleMonitoringRef',
+           'MonitoredVehicleJourney/LineRef',
+           'MonitoredVehicleJourney/DirectionRef',
+           'MonitoredVehicleJourney/FramedVehicleJourneyRef/DataFrameRef',
+           'MonitoredVehicleJourney/FramedVehicleJourneyRef/DatedVehicleJourneyRef',
+           'MonitoredVehicleJourney/PublishedLineName',
+           'MonitoredVehicleJourney/OperatorRef',
+           'MonitoredVehicleJourney/VehicleFeatureRef',
+           'MonitoredVehicleJourney/OriginRef',
+           'MonitoredVehicleJourney/OriginName',
+           'MonitoredVehicleJourney/DestinationRef',
+           'MonitoredVehicleJourney/DestinationName',
+           'MonitoredVehicleJourney/OriginAimedDepartureTime',
+           'MonitoredVehicleJourney/Monitored',
+           'MonitoredVehicleJourney/InPanic',
+           'MonitoredVehicleJourney/VehicleLocation/Longitude',
+           'MonitoredVehicleJourney/VehicleLocation/Latitude',
+           'MonitoredVehicleJourney/Bearing',
+           'MonitoredVehicleJourney/Delay',
+           'MonitoredVehicleJourney/VehicleRef'
+          ]
 
-tags = ['RecordedAtTime',
-        'ValidUntilTime',
-        'VehicleMonitoringRef',
-        'MonitoredVehicleJourney/LineRef',
-        'MonitoredVehicleJourney/DirectionRef',
-        'MonitoredVehicleJourney/FramedVehicleJourneyRef/DataFrameRef',
-        'MonitoredVehicleJourney/FramedVehicleJourneyRef/DatedVehicleJourneyRef',
-        'MonitoredVehicleJourney/PublishedLineName',
-        'MonitoredVehicleJourney/OperatorRef',
-        'MonitoredVehicleJourney/VehicleFeatureRef',
-        'MonitoredVehicleJourney/OriginRef',
-        'MonitoredVehicleJourney/OriginName',
-        'MonitoredVehicleJourney/DestinationRef',
-        'MonitoredVehicleJourney/DestinationName',
-        'MonitoredVehicleJourney/OriginAimedDepartureTime',
-        'MonitoredVehicleJourney/Monitored',
-        'MonitoredVehicleJourney/InPanic',
-        'MonitoredVehicleJourney/VehicleLocation/Longitude',
-        'MonitoredVehicleJourney/VehicleLocation/Latitude',
-        'MonitoredVehicleJourney/Bearing',
-        'MonitoredVehicleJourney/Delay',
-        'MonitoredVehicleJourney/VehicleRef'
-       ]
+not_wanted = ['MonitoredVehicleJourney',
+              'FramedVehicleJourneyRef',
+              'VehicleLocation'
+             ]
+
+expected = []
+for tag in wanted + not_wanted:
+  expected.append(re.sub(r'.*/', '', tag))
 
 def lookup_tag(tag, base):
   tag = 'siri:' + tag
@@ -39,6 +47,8 @@ def lookup_tag(tag, base):
     return base.find(tag, ns).text
   except AttributeError:
     return 'NULL'
+
+serial = 0;
 
 for directory in sys.argv[1:]:
 
@@ -55,12 +65,18 @@ for directory in sys.argv[1:]:
     tree = ET.parse(filename)
     root = tree.getroot()
 
-    serial = 0;
+
     base = 'siri:ServiceDelivery/siri:VehicleMonitoringDelivery/siri:VehicleActivity'
     for report in root.findall(base, ns):
 
       result = [str(serial),our_timestamp]
-      for tag in tags:
+      for tag in wanted:
         result.append(lookup_tag(tag,report))
       print('|'.join(result))
       serial += 1
+
+      # Check for unexpected tage
+      for element in report.findall('.//*'):
+        tag = re.sub(r'{.*}', '', element.tag)
+        if tag not in expected:
+          print("Unexpected tag '%s' in %s" % (tag, filename))
